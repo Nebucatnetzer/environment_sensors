a743155b38ba2a1cbd76f9d646b27cd6eb4a27fe
Andreas Zweili <andreas@2li.ch>
2019-10-13 19:37:20 +0100

    remove the admin and auth application
    The application should simply record the sensor values and display them in a
    pretty graph. There's nothing that would require administration or a login.

--- sensors/sensors/settings/base.py
+++ sensors/sensors/settings/base.py
@@ -25,8 +25,6 @@ DEBUG = False
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
