01f02ca4d3db24e5cb039a897fdd1cdadfa12a80
Andreas Zweili <andreas@2li.ch>
2019-10-14 21:55:00 +0200

    fix the db test

--- sensors/collector/tests/test_db.py
+++ sensors/collector/tests/test_db.py
@@ -1,13 +1,15 @@
 import datetime
 import pytest
 from django.test import TestCase
+from mixer.backend.django import mixer
 
-from collector.models import Time, Temperatur, Humidity
+from collector.models import Time, Temperature, Humidity
 
 pytestmark=pytest.mark.django_db
 
 
-def test_create_temperatur():
-    time = Time.objects.create(value=datetime.datetime.now())
-    temp = Temperatur.objects.create(value=20.0, time=time)
-    assert False
+def test_db_objects():
+    time = mixer.blend('collector.Time')
+    temp = mixer.blend('collector.Temperature', time=time)
+    humidity = mixer.blend('collector.Humidity', time=time)
+    assert time and temp and humidity
