b6ad61ccce4ea4a5fe117205412bba500acda2b2
Andreas Zweili <andreas@2li.ch>
2019-10-20 21:39:03 +0200

    create a view to display the charts

--- sensors/collector/views.py
+++ sensors/collector/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from . import plot
+from . import collector
 
-# Create your views here.
+def index_view(request):
+    collector.values_to_db()
+    plot_temp = plot.temperature()
+    plot_humidity = plot.humidity()
+    plot_pressure = plot.pressure()
+    return render(request, "collector/index.html",
+                  context={'plot_temp': plot_temp,
+                           'plot_humidity': plot_humidity,
+                           'plot_pressure': plot_pressure})
