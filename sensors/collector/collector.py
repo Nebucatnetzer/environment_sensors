30a9c3a77d39623a1444dcca61b7bb789608dcc7
Andreas Zweili <andreas@2li.ch>
2019-10-20 21:23:05 +0200

    add a function to collect and write all values to the DB

--- sensors/collector/collector.py
+++ sensors/collector/collector.py
@@ -1,9 +1,12 @@
 import os
+import datetime
 
 if os.uname()[4].startswith("arm"):
     from sense_hat import SenseHat
 else:
     from sense_emu import SenseHat
+
+from .models import Time, Temperature, Humidity, Pressure
 
 sense = SenseHat()
 
@@ -21,3 +24,10 @@ def get_pressure():
 def get_humidity():
     raw_humidity = sense.get_humidity()
     return round(raw_humidity, 1)
+
+
+def values_to_db():
+    time = Time.objects.create(value=datetime.datetime.now())
+    Temperature.objects.create(value=get_temperature(), time=time)
+    Humidity.objects.create(value=get_humidity(), time=time)
+    Pressure.objects.create(value=get_pressure(), time=time)
