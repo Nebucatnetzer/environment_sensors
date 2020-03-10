433a177e0cad5fbb244f4fcb7e6259629a5798eb
Andreas Zweili <andreas@2li.ch>
2020-03-10 20:23:43 +0100

    add migration scripts

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-nohub gunicorn sensors.wsgi:application --bind 0.0.0.0:80 --workers 3
+export DJANGO_SETTINGS_MODULE=sensors.settings.production
+export DJANGO_DEBUG=False
+export DJANGO_SECRET_KEY=foo
+./manage.py makemigrations
+./manage.py migrate
+nohub gunicorn sensors.wsgi:application --bind 0.0.0.0:8000 --workers 3
 nohub ./collector.sh
