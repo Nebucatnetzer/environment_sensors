9ba6aef42024215e1895a3a69eba4de560b434ff
Andreas Zweili <andreas@2li.ch>
2020-03-10 20:25:15 +0100

    replace nohub with &

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ export DJANGO_DEBUG=False
 export DJANGO_SECRET_KEY=foo
 ./manage.py makemigrations
 ./manage.py migrate
-nohub gunicorn sensors.wsgi:application --bind 0.0.0.0:8000 --workers 3
-nohub ./collector.sh
+gunicorn sensors.wsgi:application --bind 0.0.0.0:8000 --workers 3 &
+./collector.sh &
