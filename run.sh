06245632c7a4197df8672197f4199af5114b346a
Andreas Zweili <andreas@2li.ch>
2020-03-10 21:22:47 +0100

    remove makemigrations from run command

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 export DJANGO_SETTINGS_MODULE=sensors.settings.production
 export DJANGO_DEBUG=False
 export DJANGO_SECRET_KEY=foo
-./manage.py makemigrations
 ./manage.py migrate
 gunicorn sensors.wsgi:application --bind 0.0.0.0:8000 --workers 3 &
 ./collector.sh &
