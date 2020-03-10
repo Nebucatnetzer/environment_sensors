2700e366f26f91b83af91b350c81cf05cb1f2d92
Andreas Zweili <andreas@2li.ch>
2020-03-10 21:50:07 +0100

    let the gunicorn run in foreground

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ export DJANGO_SETTINGS_MODULE=sensors.settings.production
 export DJANGO_DEBUG=False
 export DJANGO_SECRET_KEY=foo
 ./manage.py migrate
-gunicorn sensors.wsgi:application --bind 0.0.0.0:8000 --workers 3 &
 ./collector.sh &
+gunicorn sensors.wsgi:application --bind 0.0.0.0:8000 --workers 4
